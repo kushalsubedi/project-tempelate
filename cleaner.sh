9e748f3707e134babfb9bcd5a1cb0147d1923ada
kushal <kushal@Debian.ip6.wlink.com.np>
2024-03-14 15:51:10 +0545

    managed folders

--- cleaner.sh
+++ cleaner.sh
@@ -1,5 +1,5 @@
 for file in *; do
-    if [ "${file##*.}" != "tex" ] && [ "${file##*.}" != "bib" ] && [ "$file" != "images" ]&&[ "${file##*.}" != "sh" ]; then
+    if [ "${file##*.}" != "tex" ] && [ "${file##*.}" != "bib" ] && [ "$file" != "images" ]&&[ "${file##*.}" != "sh" ] &&[ "${main.}" != "pdf" ]; then
         rm "$file"
         echo "Deleted: $file"
     fi
